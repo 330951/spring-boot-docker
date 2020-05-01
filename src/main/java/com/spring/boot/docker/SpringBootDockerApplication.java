c93f09f35639fd3ef40726278bcdcf4fae21fd24
Vijay <Vijaya.mukthala@wipro.com>
2020-05-01 11:14:08 +0530

    initial commit

---
+++ src/main/java/com/spring/boot/docker/SpringBootDockerApplication.java
@@ -0,0 +1,22 @@
+package com.spring.boot.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerApplication.class, args);
+	}
+
+	@GetMapping("/springBootDocker")
+	public String getString() {
+		System.out.println("This is from the SpringBootDockerApplication");
+
+		return "This is from the SpringBootDockerApplication";
+	}
+}
